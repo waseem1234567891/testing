e430b96ea3cf756ff116af2abf9c6b3cd578ad7e
waseem1234567891 <wasimtuff@gmail.com>
2024-05-15 16:41:08 +0500

    Update TestRunner.java

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -1,13 +1,1 @@
-package Runner;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/java/featchers/login1.feature", // Path to your feature files
-        glue = "test.Steps",dryRun = false
-)
-public class TestRunner {
-}
